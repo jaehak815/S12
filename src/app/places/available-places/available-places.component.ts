24156413ed7608778e491c7b37fafda1de2dbc18
Jay <rright815@gmail.com>
2025-09-18 00:43:20 +0800

    227

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -56,7 +56,7 @@ export class AvailablePlacesComponent implements OnInit {
 
   onSelectPlace(selectedPlace: Place) {
     this.httpClient
-      .put('http:localhost:3000/user-places', {
+      .put('http://localhost:3000/user-places', {
         placeId: selectedPlace.id,
       })
       .subscribe({
