65e12aa26ffdefafcd0bd972808bb07f5c163bd4
Jay <rright815@gmail.com>
2025-09-18 11:38:24 +0800

    229

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -6,7 +6,6 @@ import { Place } from '../place.model';
 import { HttpClient } from '@angular/common/http';
 import { PlacesService } from '../places.service';
 
-
 @Component({
   selector: 'app-user-places',
   standalone: true,
@@ -15,28 +14,26 @@ import { PlacesService } from '../places.service';
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent implements OnInit {
-  places = signal<Place[] | undefined>(undefined);
-    isFetching = signal(false);
-    error = signal('');
-    private placesService = inject(PlacesService);
-    private destroyRef = inject(DestroyRef);
+  
+  isFetching = signal(false);
+  error = signal('');
+  private placesService = inject(PlacesService);
+  private destroyRef = inject(DestroyRef);
+  places = this.placesService.loadedUserPlaces;
 
   ngOnInit() {
-      this.isFetching.set(true);
-      const subscription = this.placesService.loadUserPlaces().subscribe({
-          next: (places) => {
-            this.places.set(places);
-          },
-          error: (error: Error) => {
-            this.error.set(error.message);
-          },
-          complete: () => {
-            this.isFetching.set(false);
-          },
-        });
-  
-      this.destroyRef.onDestroy(() => {
-        subscription.unsubscribe();
-      });
-    }
+    this.isFetching.set(true);
+    const subscription = this.placesService.loadUserPlaces().subscribe({
+      error: (error: Error) => {
+        this.error.set(error.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
